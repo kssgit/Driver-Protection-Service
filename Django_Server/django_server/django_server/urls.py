fb5fcc8bba6c95a5f51ef492812be4f707e01d97
kssgit <76714467+kssgit@users.noreply.github.com>
2021-05-07 16:54:51 +0900

    create django project
    장고 프로젝트 생성

--- Django_Server/django_server/django_server/urls.py
+++ Django_Server/django_server/django_server/urls.py
@@ -1,4 +1,4 @@
-"""ai_Server URL Configuration
+"""django_server URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.0/topics/http/urls/
--- Django_Server/django_server/manage.py
+++ Django_Server/django_server/manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ai_Server.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_server.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
