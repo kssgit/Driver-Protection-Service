e7ef437101382fa44f64a14e7a0607ec3e7fef2b
he-reme <rlagpfla1017@naver.com>
2021-05-11 17:14:49 +0900

    사용자 데이터 분석 django 서버 기본 구축

---
+++ BigData/UserService/analysisApp/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from analysisApp.models import User, Co2, Eye, Emotion
+
+admin.site.register(User)
+admin.site.register(Co2)
+admin.site.register(Eye)
+admin.site.register(Emotion)
+
+# Register your models here.
---
+++ BigData/UserService/analysisApp/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+class User(models.Model) :
+    user_id = models.CharField(max_length=50, primary_key=True)
+    user_pwd = models.CharField(max_length=50)
+    name = models.CharField(max_length=50)
+    birth = models.CharField(max_length=50)
+    phone_number = models.CharField(max_length=50)
+    email = models.CharField(max_length=50)
+    gender = models.CharField(max_length=50)
+    car_kind = models.CharField(max_length=50)
+    create_time = models.DateField(auto_now_add=True)
+    serial_no1 = models.CharField(max_length=50)
+    serial_no2 = models.CharField(max_length=50)
+
+class Co2(models.Model) :
+    time = models.DateTimeField(auto_now_add=True)
+    amount = models.IntegerField()
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+
+class Eye(models.Model) :
+    time = models.DateTimeField(auto_now_add=True)
+    is_sleep = models.IntegerField()
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+
+class Emotion(models.Model) :
+    time = models.DateTimeField(auto_now_add=True)
+    emotion = models.IntegerField() # 7가지
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+# Create your models here.
