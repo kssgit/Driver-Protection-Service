a128e99ac2382aef0eedeacbcb3e5b267fbe7bc3
kssgit <76714467+kssgit@users.noreply.github.com>
2021-05-12 14:13:55 +0900

    update views and README

--- BigData/UserService/analysisApp/views.py
+++ BigData/UserService/analysisApp/views.py
@@ -1,8 +1,8 @@
-from rest_framework import serializers
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import User,Co2,Eye,Emotion
 from .UserSerializer import UserSerializer
+from .UserDataSerializer import UserDataSerializer
 import json
 
 # 회원 운전 위험수치 가져오기
@@ -23,9 +23,12 @@ def userData(request,userid):
 def createUser(request):
     # 역 직렬화
     serializer = UserSerializer(data= request.data)
+    # 사용자 비밀번호 암호화를 해야 하나??
     if(serializer.is_valid()):
         serializer.save()
-    return Response('Success')
+    else:
+        return Response({'success':False})
+    return Response({'success':True})
 
 
 #사용자 아이디 중복 체크
@@ -34,9 +37,9 @@ def userIdcheck(request,userid):
     user_id =userid
     userIdcheck = User.objects.get(user_id=user_id)
     if userIdcheck :
-        return Response('1')
+        return Response({'result':'1'})
     else:
-        return Response('0')
+        return Response({'result':'0'})
 
 
 # 로그인 
@@ -49,9 +52,9 @@ def login(request):
     if idcheck:
         # 아이디 존재
         if user_pwd == idcheck['user_pwd']:
-            return Response({'user_id':user_id,'result':True})
+            return Response({'user_id':user_id,'success':True})
         else:
-            return Response('비밀번호가 일치하지 않습니다.')
+            return Response( {'message':'비밀번호가 일치하지 않습니다.','success':False})
     else:
-        return Response('존재하지 않는 아이디')
+        return Response({'message':'존재하지 않는 아이디','success':False})
 
