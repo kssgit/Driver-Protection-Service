769487e80d552d051a8d03e4ea25e6ddf8c5bbca
he-reme <rlagpfla1017@naver.com>
2021-05-13 13:39:58 +0900

    안드로이드 앱 생성 및 기본 UI

---
+++ DPS/app/src/main/java/com/example/dps/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.dps;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button login_button;
+    Button join_button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //  로그인 버튼 클릭
+        login_button = findViewById(R.id.login_button);
+        login_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                //  1. 옳은 로그인인지 판단
+
+                // 1-1. 옳은 로그인이 아니면
+
+                //  1-2. 옳은 로그인이면
+
+                // 인텐트 선언 : 현재 액티비티, 넘어갈 액티비티
+                Intent intent = new Intent(MainActivity.this, AnalysisActivity.class);
+                // 인텐트 실행
+                startActivity(intent);
+            }
+        });
+
+        //  회원가입 버튼 클릭
+        join_button = findViewById(R.id.join_button);
+        join_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                // 인텐트 선언 : 현재 액티비티, 넘어갈 액티비티
+                Intent intent = new Intent(MainActivity.this, JoinActivity.class);
+                // 인텐트 실행
+                startActivity(intent);
+            }
+        });
+    }
+}
