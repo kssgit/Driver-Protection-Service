b55172c2511187e0b247fd17ac97c930a180a745
kssgit <76714467+kssgit@users.noreply.github.com>
2021-05-14 15:28:17 +0900

    retrofit success

--- BigData/UserService/analysisApp/models.py
+++ BigData/UserService/analysisApp/models.py
@@ -8,7 +8,6 @@ class User(models.Model) :
     phone_number = models.CharField(max_length=50)
     email = models.CharField(max_length=50)
     gender = models.CharField(max_length=50)
-    car_kind = models.CharField(max_length=50)
     create_time = models.DateField(auto_now_add=True)
     serial_no1 = models.CharField(max_length=50)
     serial_no2 = models.CharField(max_length=50)
--- DPS/app/src/main/java/com/example/dps/RetrofitAPI.java
+++ DPS/app/src/main/java/com/example/dps/RetrofitAPI.java
@@ -6,35 +6,31 @@ import java.util.List;
 
 import okhttp3.ResponseBody;
 import retrofit2.Call;
-import retrofit2.http.GET;
-import retrofit2.http.POST;
-import retrofit2.http.Path;
+import retrofit2.http.*;
 
+import com.example.dps.vo.JoinVo;
+import com.example.dps.vo.LoginVo;
 
 public interface RetrofitAPI {
 
-    public static final String REGIST_URL = "https://127.0.0.1:8080/";
+    public static final String REGIST_URL = "https://10.0.2.2:8000/";
 
     @GET("api/userdata/{userid}")
     Call<ResponseBody> getUserdata(@Path("userid") String userid);
 
 
-//    @FormUrlEncoded
-//    @POST("api//login/")
-//    Call<String> getLogin(
-//            @Field("user_id") String user_id,
-//            @Field("user_pwd") String user_pwd
-//    );
-//
-////    Call<String> getLogin(@Header("_token") String csrftoken ,@Field("user_id") String user_id,@Field("user_pwd") String user_pwd);
-//
-//    @FormUrlEncoded
-//    @POST("api/createuser/")
-//    Call<String> getUserdata( @FieldMap HashMap<String,Object> param);
-//
-//    @GET("api/userdata/{user_id}")
-//    Call<ResponseBody> getUser_data(@Path("user_id") String user_id);
 
+    @POST("api/login/")
+    Call<ResponseBody> getLogin(
+            @Header("X-CSRFTOKEN") String csrftoken,
+            @Body LoginVo loginVo
+    );
+
+    @POST("api/createuser/")
+    Call<ResponseBody> getJoin(
+            @Header("X-CSRFTOKEN") String csrftoken,
+            @Body JoinVo joinVo
+            );
 
 
 
