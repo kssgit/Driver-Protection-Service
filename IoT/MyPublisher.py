f52f363243dbe85662cf302737f1c7d69cf371b4
Seungho Lee <60536427+dltmdgh1994@users.noreply.github.com>
2021-05-18 16:25:46 +0900

    Update IoT

--- IoT/MyPublisher.py
+++ IoT/MyPublisher.py
@@ -1,7 +1,26 @@
 import paho.mqtt.publish as publish
 import cv2
+import time
 
-img = cv2.imread('C:/Users/s_csmscox/jupyterSave/eye_blink/face4.jpg')
-byteArr = bytearray(img)
+cap = cv2.VideoCapture('C:/Users/s_csmscox/jupyterSave/eye_blink/face5.mp4')
+total_start = time.time()
+frame = 0
 
-publish.single("mydata/img", byteArr, hostname="3.35.174.45")
+while True:
+    ret, img = cap.read()
+    start = time.time()
+    if not ret:
+        break
+
+    if frame % 6 == 0:
+        byteArr = bytearray(img)
+        publish.single("mydata/img", byteArr, hostname="13.208.255.135")
+        print("", frame)
+        print(time.time() - start)
+        time.sleep(0.2)
+
+    frame += 1
+
+
+
+print("total_time :", time.time() - total_start)
--- IoT/Sleep_sub.py
+++ IoT/Sleep_sub.py
@@ -14,12 +14,11 @@ from PIL import Image
 
 # 이산화탄소 subscribe
 class MyMqtt_Sub:
-
     def __init__(self):
         client = mqtt.Client()
         client.on_connect = self.on_connect
         client.on_message = self.on_message
-        client.connect("3.35.174.45", 1883, 60)  # EC2 mqttbroker 주소 
+        client.connect("13.208.255.135", 1883, 60)  # EC2 mqttbroker 주소
         ##############################
         #GPIO 설정
         # GPIO.setmode(GPIO.BCM)
@@ -32,12 +31,27 @@ class MyMqtt_Sub:
             './shape_predictor_68_face_landmarks.dat')
         self.img_size = (32, 32)
 
-        self.model = load_model('C:/Users/s_csmscox/jupyterSave/eye_blink/eye_blink_CNN_ImgGen1_FT.h5')
+        with tf.device('/cpu:0'):
+            self.model = load_model('C:/Users/s_csmscox/jupyterSave/eye_blink/eye_blink_CNN_ImgGen1_FT.h5')
+
         self.origin_img = None
 
         self.eye_blink = None
         self.co2 = None
         self.motion = None
+
+        self.frame = 0
+        self.eye_alert_count = 0
+
+        self.nose_sum = 0
+        self.nose_cnt = 0
+        self.nose_mean = 0
+        self.nose_alert_cnt = 0
+
+        self.mouse_sum = 0
+        self.mouse_cnt = 0
+        self.mouse_mean = 0
+        self.mouse_alert_cnt = 0
 
         ###########################
         client.loop_forever()
@@ -52,6 +66,9 @@ class MyMqtt_Sub:
 
     def on_message(self, client, userdata, msg):
 
+        # start = time.time()
+        self.frame += 1
+
         myval = msg.payload
 
         # 움직임 탐지 방식 => 원본 이미지와 새로운 이미지 사이의 달라진 픽셀 측정
@@ -61,24 +78,19 @@ class MyMqtt_Sub:
             myval = np.frombuffer(myval, np.uint8)
             # reshape 해줘야 한다.
             # myval = np.fromstring(myval, np.uint8)
-            myval = myval.reshape(256, 512, 3)
+            myval = myval.reshape(852, 480, 3)
 
             if self.origin_img is None:
                 self.origin_img = myval
-                cv2.imshow('image', self.origin_img)
-                cv2.waitKey()
-                cv2.destroyAllWindows()
             else:
-                compare_img = myval
-                # 이산화탄소 데이터 / 이미지 파일 구분?
                 faces = self.detector(myval)
 
                 for face in faces:
-                    shapes = self.predictor(compare_img, face)
+                    shapes = self.predictor(myval, face)
                     shapes = face_utils.shape_to_np(shapes)
 
-                    eye_img_l, eye_rect_l = self.crop_eye(compare_img, eye_points=shapes[36:42])
-                    eye_img_r, eye_rect_r = self.crop_eye(compare_img, eye_points=shapes[42:48])
+                    eye_img_l, eye_rect_l = self.crop_eye(myval, eye_points=shapes[36:42])
+                    eye_img_r, eye_rect_r = self.crop_eye(myval, eye_points=shapes[42:48])
 
                     eye_img_l = cv2.resize(eye_img_l, dsize=self.img_size)
                     eye_img_r = cv2.resize(eye_img_r, dsize=self.img_size)
@@ -88,7 +100,8 @@ class MyMqtt_Sub:
                         np.float32)
                     eye_input_l = eye_input_l / 255
 
-                    pred_l = self.model.predict(eye_input_l)
+                    with tf.device('/cpu:0'):
+                        pred_l = self.model.call(tf.convert_to_tensor(eye_input_l), training = False)
                     pred_l = np.argmax(pred_l)
 
                     # 오른쪽 눈
@@ -96,48 +109,55 @@ class MyMqtt_Sub:
                         np.float32)
                     eye_input_r = eye_input_r / 255
 
-                    pred_r = self.model.predict(eye_input_r)
+                    with tf.device('/cpu:0'):
+                        pred_r = self.model.call(tf.convert_to_tensor(eye_input_r), training = False)
                     pred_r = np.argmax(pred_r)
-
-                    # 눈에 직사각형 그리기
-                    # cv2.rectangle(compare_img, pt1=tuple(eye_rect_l[0:2]), pt2=tuple(eye_rect_l[2:4]),
-                    #               color=(255, 255, 255), thickness=2)
-                    # cv2.rectangle(compare_img, pt1=tuple(eye_rect_r[0:2]), pt2=tuple(eye_rect_r[2:4]),
-                    #               color=(255, 255, 255), thickness=2)
-                    #
-                    # cv2.putText(compare_img, str(pred_l), tuple(eye_rect_l[0:2]), cv2.FONT_HERSHEY_SIMPLEX, 0.7,
-                    #             (255, 255, 255), 2)
-                    # cv2.putText(compare_img, str(pred_r), tuple(eye_rect_r[0:2]), cv2.FONT_HERSHEY_SIMPLEX, 0.7,
-                    #             (255, 255, 255), 2)
-
-                    cv2.imshow('image', compare_img)
-                    cv2.waitKey()
-                    cv2.destroyAllWindows()
 
                     # 두 눈 다 감은 경우 졸음으로 예측
                     if pred_l == 0 and pred_r == 0:
-                        n_count += 1
+                        self.eye_alert_count += 1
                     else:
-                        n_count = 0
+                        self.eye_alert_count = 0
                         self.eye_blink = 0
 
-                    # n_count가 10 초과하면 경고 메세지
-                    if n_count > 10:
-                        # cv2.putText(compare_img, "Wake up!(eye_blink)", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 0.5,
-                        #             (0, 0, 255), 2)
+                    # eye_alert_count가 10 초과하면 경고 메세지
+                    if self.eye_alert_count > 10:
                         print("Wake up!(eye_blink)")
                         self.eye_blink = 1
 
-                    # 원본과 움직인 이미지를 비교해서 mse가 일정 이상일 시 움직인것으로 판단
-                    mse_val = self.mse(self.origin_img, compare_img)
-
-                    if mse_val > 2000:
-                        # cv2.putText(compare_img, "Wake up!(movement)", (50, 100), cv2.FONT_HERSHEY_SIMPLEX, 0.5,
-                        #             (255, 0, 0), 2)
-                        print("Wake up!(movement)")
-                        self.motion = 1
+                    # 코 끝 특징점이 기존에 비해 내려가면 고개 내려갔다고 판정?
+                    if self.nose_cnt == 0:
+                        self.nose_sum += shapes[33][1]
+                        self.nose_cnt += 1
+                        self.nose_mean = self.nose_sum / self.nose_cnt
+                    elif shapes[33][1] - self.nose_mean > 10:
+                        self.nose_alert_cnt += 1
                     else:
-                        self.motion = 0
+                        self.nose_sum += shapes[33][1]
+                        self.nose_cnt += 1
+                        self.nose_mean = self.nose_sum / self.nose_cnt
+                        self.nose_alert_cnt = 0
+
+                    if self.nose_alert_cnt > 10:
+                        print("Wake up!(movement)")
+
+                    # 입 특징점 기반으로 하품 감지
+                    mouse_rate = (shapes[57][1] - shapes[51][1]) / (shapes[54][0] - shapes[48][0])
+
+                    if self.mouse_cnt == 0:
+                        self.mouse_sum += mouse_rate
+                        self.mouse_cnt += 1
+                        self.mouse_mean = self.mouse_sum / self.mouse_cnt
+                    elif mouse_rate - self.mouse_mean > 0.2:
+                        self.mouse_alert_cnt += 1
+                    else:
+                        self.mouse_sum += mouse_rate
+                        self.mouse_cnt += 1
+                        self.mouse_mean = self.mouse_sum / self.mouse_cnt
+                        self.mouse_alert_cnt = 0
+
+                    if self.mouse_alert_cnt > 5:
+                        print("Wake up!(yawning)")
 
         elif myval == 'Co2_data':
             if myval > 1500:
@@ -146,7 +166,9 @@ class MyMqtt_Sub:
             else:
                 self.co2 = 0
 
-        self.sleep_gate(self, self.eye_blink, self.motion, self.co2)
+        # self.sleep_gate(self, self.eye_blink, self.motion, self.co2)
+        # print("time :", time.time() - start)
+        print("", self.frame)
 
     @staticmethod
     def sleep_gate(self, eye_blink, motion, co2):
